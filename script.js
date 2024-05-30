5bf93ba0a1daf3693976d41f7db51e6ff47b5c07
Jordy Vasquez <Jordyvasquez61@gmail.com>
2024-05-30 18:22:48 -0500

    primer comit

---
+++ script.js
@@ -0,0 +1,11 @@
+let A = 4;
+let B = 5;
+let C = 1;
+
+let resultado1 = ((B * A) - B ^ 2) / 4 * C;
+let resultado2 = (A * B) / 3 ^ 2;
+let resultado3 = (((B + C) / 2 * A * 10) * 3 * B) - 6;
+
+console.log("a. B * A – B ^ 2 / 4 * C Resultado: " + resultado1)
+console.log("(A * B) / 3 ^ 2 Resultado: " + resultado2)
+console.log("(((B + C) / 2 * A + 10) * 3 * B) – 6 Resultado: " + resultado3)
